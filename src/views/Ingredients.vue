a67920346d26995766f77f2e025fa357ede81087
Richard Chavez <thecovermantaco63@gmail.com>
2025-12-03 19:54:49 -0600

    Ingredients page working

--- src/views/Ingredients.vue
+++ src/views/Ingredients.vue
@@ -16,11 +16,33 @@ onMounted(() => {
 
 })
 
+const searchIngredients = computed(() => {
+  if (!searchIngredients) return ingredients
+
+  return ingredients.value.filter( (i) => i.strIngredient.toLowerCase().includes(keyword.value.toLowerCase()))
+})
+
 </script>
 <template>
   <div class="p-8 pb-0 text-orange-500">
     <h1 class="text-4xl font-bold mb-4"> Search Meals By Ingredient</h1>
   </div> 
 
-  {{ ingredients }}
+  <!-- {{ ingredients }} -->
+  <div>
+    <input type="text" v-model="keyword" class="rounded border-2 border-gray-200 w-full bg-white mb-5" placeholder="Enter ingredient name"/>
+
+    <div class="grid grid-cols-4 gap-5">
+      <router-link 
+        v-for="ingredient in searchIngredients" 
+        :key="ingredient.idIngregient" 
+        :to="{name: 'MealsByIngredients', params: {ingredient: ingredient.strIngredient}}" 
+        class="bg-white rounded p-3 mb-3 shadow block">
+          <img :src="`http://www.themealdb.com/images/ingredients/${ingredient.strIngredient}-small.png`"/>
+          <h3 class="text-2xl font-bold mb-2">{{ ingredient.strIngredient }}</h3>
+      </router-link>
+    </div>
+
+  </div>
+
 </template>
